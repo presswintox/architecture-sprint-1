bf3928e9ba2da3efed2f3237bf467ada5bd64461
dmitrijbikmuhametov <presswintox@gmail.com>
2025-04-07 03:12:59 +0300

    Перенос в другой каталог

--- frontend/microfrontend/card/webpack.config.js
+++ frontend/microfrontend/card/webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin.js");
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
